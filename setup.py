9c4cf7562bbd41ca76fd442a114d2bfb7d5505ee
tronzap <dev@tronzap.com>
2025-04-02 18:31:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,33 +6,31 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="tronzap-sdk",
     version="1.0.0",
-    author="Tron Energy Market",
-    author_email="support@tronzap.com",
-    description="Official Python SDK for TronZap.com API. The API lets you buy TRON energy to lower USDT (TRC20) transfer fees.",
+    description="Official Python SDK for the TronZap API. TronZap is a platform for renting energy on the TRON blockchain. Buy TRON energy for cheap USDT transfers on TronZap.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    author="Tron Energy Market",
+    author_email="support@tronzap.com",
     url="https://tronzap.com",
     project_urls={
+        "Homepage": "https://tronzap.com",
+        "Documentation": "https://docs.tronzap.com/",
         "Bug Tracker": "https://github.com/tron-energy-market/tronzap-sdk-python/issues",
         "Source Code": "https://github.com/tron-energy-market/tronzap-sdk-python",
+        "Support": "https://t.me/tronzap_bot"
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3"
     ],
+    license="MIT",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.7",
+    python_requires=">=3",
     install_requires=[
-        "requests>=2.25.0",
+        "requests>=2.25.0"
     ],
     keywords=[
         "TronZap",
